55057379c2ad6ca39f8ff4ecb174abd580d96b2e
Петров Слава <pvv56@mail.ru>
2022-09-28 21:51:04 +0300

    rebase

--- src/util/const.js
+++ src/util/const.js
@@ -18,17 +18,17 @@ export const SortType = {
   TIME: 'time',
 };
 
-export const UserAction = {
-  UPDATE_POINT: 'UPDATE_POINT',
-  ADD_POINT: 'ADD_POINT',
-  DELETE_POINT: 'DELETE_POINT',
-};
-
 export const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR',
   INIT: 'INIT',
+};
+
+export const UserAction = {
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
 };
 
 export const BLANK_POINT = {
